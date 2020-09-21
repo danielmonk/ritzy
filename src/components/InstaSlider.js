c57859dfb6fc02176b4c93759eca9d13e50cdfa7
danielmonk <danielmonk@msn.com>
2020-09-21 18:07:29 +0100

    adding slider

---
+++ src/components/InstaSlider.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+//import { Grid, Cell } from 'react-foundation';
+import { Link } from 'gatsby'
+import Fade from 'react-reveal/Fade';
+
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      arrows: false,
+      //centerMode: true,
+      //centerPadding: 100,
+      autoplay: true,
+      touchThreshold:100,
+    };
+    return (
+        <section id="insta">
+            <div className="content--wrapper content--wrapper--big">
+              <div className="column centered">
+                <Fade>
+                  <h4>Follow us on <strong><a href="https://www.instagram.com/ritzybarber" target="_blank">Instagram</a></strong><a className="insta" href="https://www.instagram.com/ritzybarber" target="_blank"></a></h4>
+                  <p>Keep up to date with all of our latest news.</p>
+                </Fade>
+              </div>
+            </div>
+                <Slider {...settings} className="slider slick-slider--video-title">
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta1})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta1Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta2})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta2Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta3})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta3Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta4})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta4Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta5})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta5Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta6})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta6Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta7})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta7Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta8})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta8Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta9})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta9Link} target="_blank" ></a>
+                </div>
+                <div>
+                    <div className="cover " style={{backgroundImage: `url(${this.props.insta10})`}} alt="Ritzy Barbers, Peterborough Skin Fade" />
+                    <a href={this.props.insta10Link} target="_blank" ></a>
+                </div>
+                </Slider>
+        </section>
+    );
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,28 @@ import insta from "../images/instagram.png"
 import facebook from "../images/facebook.png"
 import iby from "../images/iby-bw.png"
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import InstaSlider from '../components/InstaSlider';
 let instaPhoto1 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/117795878_1250199461991549_3576501364359053768_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=109&_nc_ohc=_G5PxI7NTm4AX9rZB43&oh=513fe279817825e6e0d30e7eca901d25&oe=5F894506"
-let instaPhoto2 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/109811559_586866545310110_141645576686131022_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=102&_nc_ohc=dim_S0WSpTUAX_qKXXQ&_nc_tp=18&oh=adc191591306279b50bf00f7f5045a23&oe=5F8846C0"
-let instaPhoto3 = "https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/107869648_609295649723384_3063073575809310173_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=108&_nc_ohc=BGMhbOFEIKEAX_ou-YD&oh=a394c6b661c261a15b7b3b5e6925bb9e&oe=5F896D5E"
-let instaPhoto4 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/94837887_243375443533012_3976964820012419774_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=104&_nc_ohc=nLeLJqDgAHQAX9DeMzK&oh=d569a5c2d33a8a7aae930b5435ee7196&oe=5F878262"
+let insta1Link = "https://www.instagram.com/p/CD9PcGcD_Nd/"
+let instaPhoto2 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/109811559_586866545310110_141645576686131022_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=102&_nc_ohc=cTFLx5f9H9oAX-2itLJ&_nc_tp=18&oh=c590959ebd566da74a7ee4f72116b482&oe=5F942440"
+let insta2Link = "https://www.instagram.com/p/CC36kx5jV-L/"
+let instaPhoto3 = "https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/107869648_609295649723384_3063073575809310173_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=108&_nc_ohc=KFA1s29EyIwAX_enfig&_nc_tp=15&oh=27ac359a1c64d13c2686307348704299&oe=5F91565E"
+let insta3Link = "https://www.instagram.com/p/CCl3PJVjol4/"
+let instaPhoto4 = "https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/101013839_723611708178978_1691840401939268412_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=106&_nc_ohc=ePkf91iDhJoAX_HD8kG&_nc_tp=19&oh=855f96c16d64f0b551e13dffcd6453ab&oe=5F91F86B"
+let insta4Link = "https://www.instagram.com/p/CAujdf_julb/"
+let instaPhoto5 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/97572216_1092615757781995_5210964166121146314_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=107&_nc_ohc=01yQ3H-jS3YAX_3gCEV&_nc_tp=15&oh=2e93bb5ca784c33f00f1993172c7b41a&oe=5F92092B"
+let insta5Link = "https://www.instagram.com/p/CABSzWsjSFD/"
+let instaPhoto6 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/94837887_243375443533012_3976964820012419774_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=104&_nc_ohc=hS5SKhneGWIAX8X4SHk&_nc_tp=15&oh=9275146e28285fe61c460c3ce167e510&oe=5F935FE2"
+let insta6Link = "https://www.instagram.com/p/B_k2-5IjwsF/"
+let instaPhoto7 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/94444109_534106800844687_5954877744533653086_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=107&_nc_ohc=kQt0522P7NwAX-aHubH&_nc_tp=18&oh=4e0c305bc2fa473190b758981079fc2a&oe=5F90E2A6"
+let insta7Link = "https://www.instagram.com/p/B_P3suajaLZ/"
+let instaPhoto8 = "https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/84348364_122408482546961_7316840853933633656_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&_nc_cat=105&_nc_ohc=juSIGrTOBm4AX9oSXQo&_nc_tp=18&oh=a9669465d5d0c09c829badfc81c0f1a9&oe=5F93C7D5"
+let insta8Link = "https://www.instagram.com/p/B8uP5W0g5Pi/"
+let instaPhoto9 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/82424935_151815782913748_6485747346384039280_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=104&_nc_ohc=xXPbbeFlWXEAX_kVa7B&_nc_tp=19&oh=9ba3731bc63f718b2ee6d7f0a5210763&oe=5F93AFF7"
+let insta9Link = "https://www.instagram.com/p/B7UK7tpAzmb/"
+let instaPhoto10 = "https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-15/e35/75616364_189920152056493_571939182375379545_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_cat=101&_nc_ohc=de4f6vAfvbIAX8mFquU&_nc_tp=18&oh=eaf35d61e732ba1bf234ca74f2f85a11&oe=5F93BE7D"
+let insta10Link = "https://www.instagram.com/p/B56A-1agxCN/"
+
 
 
 
@@ -267,32 +285,29 @@ class Header extends React.Component {
             </div>   
           </section>
 
-          <section id="insta">
-            <div className="content--wrapper content--wrapper--big">
-              <div className="column centered">
-                <Fade>
-                  <h4>Follow us on <strong><a href="https://www.instagram.com/ritzybarber" target="_blank">Instagram</a></strong><a className="insta" href="https://www.instagram.com/ritzybarber" target="_blank"></a></h4>
-                  <p>Keep up to date with all of our latest news.</p>
-                </Fade>
-              </div>
-            </div>
-            <div className="insta-grid">
-            <Fade>
-              <div className="insta-photo" style={styles.insta1}>
-                
-              </div>
-              <div className="insta-photo" style={styles.insta1}>
-                
-              </div>
-              <div className="insta-photo" style={styles.insta1}>
-                
-              </div>
-              <div className="insta-photo" style={styles.insta1}>
-                
-              </div>
-            </Fade>
-            </div>
-          </section>
+
+          <InstaSlider
+              insta1={instaPhoto1}
+              insta1Link = {insta1Link}
+              insta2={instaPhoto2}
+              insta2Link = {insta2Link}
+              insta3={instaPhoto3}
+              insta3Link = {insta3Link}
+              insta4={instaPhoto4}
+              insta4Link = {insta4Link}
+              insta5={instaPhoto5}
+              insta5Link = {insta5Link}
+              insta6={instaPhoto6}
+              insta6Link = {insta6Link}
+              insta7={instaPhoto7}
+              insta7Link = {insta7Link}
+              insta8={instaPhoto8}
+              insta8Link = {insta8Link}
+              insta9={instaPhoto9}
+              insta9Link = {insta9Link}
+              insta10={instaPhoto10}
+              insta10Link = {insta10Link}
+            />
 
           <section id="opening" className="opening">
             <div className="content--wrapper">
@@ -352,12 +367,18 @@ class Header extends React.Component {
 
     
           <section id="contact" className="contact">
-            <div className="content--wrapper">
+          <div className="content--wrapper">
+            <div className="column centered">
+                <div className="contact__text">
+                  <h5><a href="mailto:danieljmonk@gmail.com">Contact <strong>Us</strong></a></h5>
+                </div>
+            </div>
+          </div>
+            <div className="content--wrapper no-align">
               <div className="contact--left">
                 <div className="contact__text">
                   <Fade>
                     <p>Got a question for us?</p>
-                    <h5><a href="mailto:danieljmonk@gmail.com">Enquire Now</a></h5>
                     <form action="https://getform.io/f/81fd928c-0842-4173-923d-585cd6383e38" method="POST">
 
                     <div className="field first">
@@ -386,6 +407,9 @@ class Header extends React.Component {
                 </div>
               </div>
               <div className="contact--right">
+                <div className="contact__text">
+                  <p>Our Locations</p>
+                </div>
                 <div className="contact--map">
                   <LoadScript
                     googleMapsApiKey="AIzaSyBlbKAE_VcREMiyIGSSu0nwKZi0PvJAFUI"
