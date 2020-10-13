4d4cffe27917eca775d89ffffef30b792e46188a
daniel monk <>
2020-10-13 09:39:30 +0100

    adding gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,19 @@ module.exports = {
         icon: 'src/images/ritzy-r-no-text.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-76115201-5",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
