4f4161a9dd2357142c0af7da7442c9fdf311dc7a
daniel.monk <danielmonk@msn.com>
2022-02-04 10:10:28 +0000

    adding redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+    const {createRedirect} = actions //actions is collection of many actions - https://www.gatsbyjs.org/docs/actions
+    createRedirect({ fromPath: 'ritzystudios.com', toPath: 'ritzybarbers.com', isPermanent: true });
+  }
